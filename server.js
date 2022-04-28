1fd8c28de0e08ffbf916a883dccc25f746adbb8d
hanossef <han.ossef@gmail.com>
2022-04-28 12:16:12 +0200

    added static

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on  http://localhost:${port}`)
-})
+  console.log(`Example app listening on  http://localhost:${port}`);
+});
+
+app.use(express.static('static'));
+
+// app.use (req,res) => {
+//     res.status(404).send("Error 404: file not found");
+// };
