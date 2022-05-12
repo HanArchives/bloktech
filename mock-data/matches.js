1334560fe9b66c1e14b5fbcfb9de1ce7b2e2387f
hanossef <han.ossef@gmail.com>
2022-05-12 14:54:11 +0200

    wip:linting

--- mock-data/matches.js
+++ mock-data/matches.js
@@ -1,6 +1,7 @@
 const match = [
   {
     id: 0,
+    image: '/img/yebba.jpg',
     name: 'Charles',
     about: 'a fluffo doggo.',
     age: '2',
@@ -9,6 +10,7 @@ const match = [
   },
   {
     id: 1,
+    image: '/img/luna.jpg',
     name: 'Luna',
     about: 'A very fluffo doggo.',
     age: '4',
@@ -17,6 +19,7 @@ const match = [
   },
   {
     id: 2,
+    image: '/img/bambi.jpg',
     name: 'Bambi',
     about:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
+const url = require('url'); // hierdoor kan de URL uitgelezen worden http://localhost:3000/match?dogID=2
+// const { application } = require('express');
+// const req = require('express/lib/request');
 const matches = require('./mock-data/matches');
-let url = require('url'); // hierdoor kan de URL uitgelezen worden http://localhost:3000/match?dogID=2
-const { application } = require('express');
-const req = require('express/lib/request');
 
 // port 3000
 app.listen(port, () => {
@@ -17,20 +18,20 @@ app.set('view engine', 'ejs'); // to use ejs
 app.set('views, view');
 
 // index page render
-app.get('/index', function (req, res) {
+app.get('/index', (req, res) => {
   res.render('pages/index');
 });
 
 // match page render
-app.get('/match', function (req, res) {
-  let q = url.parse(req.url, true).query;
+app.get('/match', (req, res) => {
+  const q = url.parse(req.url, true).query;
   // default waarde geven
   if (q.dogID == null) {
     q.dogID = 2;
   }
 
   res.render('pages/match', {
-    matches: matches,
+    matches,
     dogID: q.dogID,
   });
 });
