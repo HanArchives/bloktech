cec264b265a1198e9c067b15070b2a5623800278
hanossef <han.ossef@gmail.com>
2022-05-12 12:28:32 +0200

    wip

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const matches = require('./data/matches');
-let url = require('url'); // hierdoor kan de URL uitgelezen worden http://localhost:3000/profile?dogID=2
-//const { identity } = require('lodash');
+const matches = require('./mock-data/matches');
+let url = require('url'); // hierdoor kan de URL uitgelezen worden http://localhost:3000/match?dogID=2
+const { application } = require('express');
+const req = require('express/lib/request');
 
 // port 3000
 app.listen(port, () => {
@@ -18,26 +19,32 @@ app.set('views, view');
 // index page render
 app.get('/index', function (req, res) {
   res.render('pages/index');
-  // res.render('pages/index', {
-  //   title: 'Gender?',
-  // });
 });
 
-// profile page render
-app.get('/profile', function (req, res) {
+// match page render
+app.get('/match', function (req, res) {
   let q = url.parse(req.url, true).query;
   // default waarde geven
   if (q.dogID == null) {
     q.dogID = 2;
   }
 
-  res.render('pages/profile', {
+  res.render('pages/match', {
     matches: matches,
     dogID: q.dogID,
   });
 });
 
+// redirect pagina questonnaire naar matches
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.get('/:id', (req, res, next) => {
+// const id = req.params.id;
+// const movie = find(date,function (value)
+// });
+
 // reponse 404 message when file not found
 app.use((req, res) => {
-  res.status(404).send('Error 404: File not found');
+  res.status(404).render('pages/404');
 });
