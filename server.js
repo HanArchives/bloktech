2fd500365af8f09f70e1fa2bd559c21499992cc6
hanossef <han.ossef@gmail.com>
2022-05-15 08:09:18 +0200

    wip: app.post

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
 const url = require('url'); // hierdoor kan de URL uitgelezen worden http://localhost:3000/match?dogID=2
@@ -34,11 +33,95 @@ app.get('/match', (req, res) => {
   });
 });
 
-// redirect pagina questonnaire naar matches
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 // reponse 404 message when file not found
 app.use((req, res) => {
   res.status(404).render('pages/404');
 });
+
+// redirect pagina questonnaire naar matches
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+
+//Temporary data storage
+const matchItems = [
+  {
+    id: 'female',
+    name: 'gender',
+  },
+  {
+    id: 'male',
+    name: 'gender',
+  },
+  {
+    id: 'zero-five',
+    name: 'age',
+  },
+  {
+    id: 'six-ten',
+    name: 'age',
+  },
+  {
+    id: 'ten-plus',
+    name: 'age',
+  },
+  {
+    id: 'small',
+    name: 'size',
+  },
+  {
+    id: 'medium',
+    name: 'size',
+  },
+  {
+    id: 'large',
+    name: 'size',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.render('form', {
+    matchItems,
+  });
+});
+
+app.post('/', (req, res) => {
+  const { amounts, checked } = req.body;
+
+  // check whether we have any checked entries first
+  if (Array.isArray(checked)) {
+    const selectedAmounts = checked.map((idName) => amounts[idName]);
+
+    console.log(req.body);
+    console.log(selectedAmounts);
+  }
+  // res.redirect('/');
+  res.render('pages/match.ejs');
+});
+
+// // // test 2
+// app.get('/', (req, res) => {
+//   res.render('pages/index', { data: data });
+// });
+
+// app.get('/:id', (req, res, next) => {
+//   const id = req.params.id;
+//   const match = find(data, function (value) {
+//     return value.id === id;
+//   });
+
+//   if (!match) {
+//     next();
+//     return;
+//   }
+
+//   res.render('pages/match.ejs', { data: data });
+// });
+
+// Access the parse results as request.body
+// app.post('/', function (req, res) {
+//   console.log(req.body);
+
+//   res.render('pages/match.ejs');
+// });
