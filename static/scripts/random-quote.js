a49ce263783db0c7dacf929e3bb59497d4c0e123
hanossef <han.ossef@gmail.com>
2022-05-21 22:59:37 +0200

    style change

--- static/scripts/random-quote.js
+++ static/scripts/random-quote.js
@@ -3,6 +3,7 @@
 ///////////////////////
 async function fetchQuote() {
   const url = 'https://cat-fact.herokuapp.com/facts/random';
+  //http://dog-api.kinduff.com
   let response = await fetch(url);
   let quote = await response.json();
   let randomQuote = document.querySelector('.random-quote');
